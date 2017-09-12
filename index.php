76114376829a9a2c7c011e44c0612da9271af583
Brian Lynch <bijil6339@gmail.com>
2017-09-12 17:41:27 -0400

    index ui complete added questions page

--- index.php
+++ index.php
@@ -14,6 +14,13 @@
   <main>
   	<div class="container">
   		<h2>Test Your PHP knowledge!</h2>
+  		<p>This is a quiz to test your knowledge of the PHP programming language.</p>
+  		<ul>
+  			<li><strong>Number of Questions: </strong>5</li>
+  			<li><strong>Type: </strong>Multiple Choice</li>
+  			<li><strong>Estimated Time: </strong>5 minutes</li>
+  		</ul>
+  		<a href="questions.php?n=1" class="start">Start Quiz</a>
   	</div>
   </main>
   <footer>
