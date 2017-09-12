fc86ddb4b58edf23a5c0fa4483727ae85ff9a791
Brian Lynch <bijil6339@gmail.com>
2017-09-12 16:51:54 -0400

    quizzer ui file structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Quizzer!</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+  <header>
+  	<div class="container">
+  		<h1>PHP Quizzer</h1>
+  	</div>
+  </header>
+  <main>
+  	<div class="container">
+  		<h2>Test Your PHP knowledge!</h2>
+  	</div>
+  </main>
+  <footer>
+  	<div class="container">
+  		<p>&copy; 2017 Lynchology.com</p>
+  	</div>
+  </footer>
+
+</body>
+</html>
